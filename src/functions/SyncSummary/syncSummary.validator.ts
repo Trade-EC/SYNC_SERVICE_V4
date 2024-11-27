a6780fc718eabce12e8555924b6c40a4c2836cff
Nicolas Jimenez <nicolas.jimenez@trade.ec>
2024-11-27 09:41:45 -0500

    feat: add account 37 and 39 to kfc list

--- src/functions/SyncSummary/syncSummary.validator.ts
+++ src/functions/SyncSummary/syncSummary.validator.ts
@@ -1,7 +1,7 @@
 import { z } from "/opt/nodejs/sync-service-layer/node_modules/zod";
 
 export const syncSummaryValidator = z.object({
-  vendorId: z.string(),
-  channelId: z.string(),
+  vendorId: z.string().optional(),
+  channelId: z.string().optional(),
   listId: z.string().optional()
 });
