1edecb81ddbe58ca1c720a7feb433a467bad8e74
Nicolas Jimenez <nicolas.jimenez@trade.ec>
2024-12-13 09:23:59 -0500

    feat: map sale prices for sync

--- src/layers/sync-service-layer/transforms/product.transform.ts
+++ src/layers/sync-service-layer/transforms/product.transform.ts
@@ -168,20 +168,36 @@ export const transformPrices = (
   taxesInfo: TaxesInfo | undefined,
   vendorTaxes: TaxesInfo | undefined
 ) => {
-  const { price, pointPrice, suggestedPointPrice, suggestedPrice } = priceInfo;
+  const { price, pointPrice, suggestedPointPrice, suggestedPrice, salePrice } =
+    priceInfo;
   const productPrice: any = {};
   const taxes = taxesInfo ?? vendorTaxes;
+  const netPrice = salePrice ?? price;
+  const priceBeforeSale = salePrice ? price : 0;
+  const grossPriceForSale = salePrice
+    ? calculateGrossPrice(priceBeforeSale, taxes)
+    : 0;
+
+  const salePercentage = salePrice
+    ? Math.floor((1 - netPrice / priceBeforeSale) * 10000) / 10000
+    : 0;
+  const saleValue = salePrice ? grossPriceForSale * salePercentage : 0;
 
   const normal = {
     category: "NORMAL",
     symbol: "",
-    netPrice: price,
+    netPrice: netPrice,
     taxes: getTaxes(taxes),
-    grossPrice: calculateGrossPrice(price, taxes),
+    grossPrice: calculateGrossPrice(netPrice, taxes),
     discounts: [],
     discountGrossPrice: 0,
     discountNetPrice: 0,
-    discount: 0
+    discount: 0,
+    priceBeforeSale,
+    grossPriceForSale,
+    salePercentageDecimal: salePercentage,
+    salePercentage: salePercentage * 100,
+    saleValue
   };
 
   const points = {
@@ -191,7 +207,12 @@ export const transformPrices = (
     discounts: [],
     discountGrossPrice: 0,
     discountNetPrice: 0,
-    discount: 0
+    discount: 0,
+    priceBeforeSale: 0,
+    grossPriceForSale: 0,
+    salePercentageDecimal: 0,
+    salePercentage: 0,
+    saleValue: 0
   };
 
   const suggested = {
@@ -203,7 +224,12 @@ export const transformPrices = (
     discounts: [],
     discountGrossPrice: 0,
     discountNetPrice: 0,
-    discount: 0
+    discount: 0,
+    priceBeforeSale: 0,
+    grossPriceForSale: 0,
+    salePercentageDecimal: 0,
+    salePercentage: 0,
+    saleValue: 0
   };
 
   const suggestedPoints = {
@@ -213,7 +239,12 @@ export const transformPrices = (
     discounts: [],
     discountGrossPrice: 0,
     discountNetPrice: 0,
-    discount: 0
+    discount: 0,
+    priceBeforeSale: 0,
+    grossPriceForSale: 0,
+    salePercentageDecimal: 0,
+    salePercentage: 0,
+    saleValue: 0
   };
 
   productPrice["NORMAL"] = normal;
